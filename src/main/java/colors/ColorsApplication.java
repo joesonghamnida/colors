85c79d1b6dbfc3461277378927852bb0c0ea27dc
Joe Morris <joe.morris.dev@gmail.com>
2017-05-25 18:30:13 -0400

    established basic packages

---
+++ src/main/java/colors/ColorsApplication.java
@@ -0,0 +1,12 @@
+package colors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorsApplication.class, args);
+	}
+}
